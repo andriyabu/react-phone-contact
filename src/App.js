96f112de11ce91a48e1e768bc1e97d3daefbf9a4
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 14:53:13 +0700

    created navbar component Navbar.js and import it from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-
-const Nakama = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <p>My Name is <b>{props.nama}</b></p>
-      <p>My Power is <b>{props.power}</b></p>
-    </div>
-  )
-}
+import Navbar from "./Navbar";
 
 function App() {
-  const greetings = "Helloo Nakama";
   return (
     <div className="App">
-     <h1>{greetings}</h1>
-     <Nakama nama="Monkey D. Luffi" power="Gatling Gun"/>
-     <Nakama nama="Roronoa Zoro" power="Santoryu" />
+     <Navbar />
+     
     </div>
   );
 }
