31fca73a6201a129879f956e3b76bca0f57272dd
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 15:00:03 +0700

    created Component Home.js and Create.js and import it on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Home />
+     <Create />
      
     </div>
   );
