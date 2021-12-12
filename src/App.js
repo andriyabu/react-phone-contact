d42db24da5e7a271d9fb6491c5c5df26bf6403f3
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 14:18:27 +0700

    Functional Class with arrow function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+
+const Name = () => {
+  return(
+    <div>
+      <p>My Name is <b>Monkey D. Lufi</b></p>
+      <p>My Power is <b>Gomu gomu</b></p>
+    </div>
+  )
+}
+
 function App() {
   const greetings = "Helloo Nakama";
   return (
     <div className="App">
      <h1>{greetings}</h1>
+
+     <Name />
     </div>
   );
 }
