b264cd206e70d7ed3eee0a2487625a5b35bc876b
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 16:05:20 +0700

    event onclick handler implementation

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Halloo");
+    }
+
+    const handleClick2 = (name) => {
+        console.log(name)
+    } 
     return (
         <div className="home">
             <h3>Contact List</h3>
+            <button onClick={handleClick}>klik</button>
+            <button onClick={() => handleClick2("Luffi")}>Klick Lagi</button>
         </div>
     );
 }
