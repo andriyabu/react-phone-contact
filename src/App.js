49d549541dff4655354368b299cbe494de2d90db
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 14:35:13 +0700

    created props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 
-const Name = () => {
+const Nakama = (props) => {
+  console.log(props)
   return(
     <div>
-      <p>My Name is <b>Monkey D. Lufi</b></p>
-      <p>My Power is <b>Gomu gomu</b></p>
+      <p>My Name is <b>{props.nama}</b></p>
+      <p>My Power is <b>{props.power}</b></p>
     </div>
   )
 }
@@ -15,8 +16,8 @@ function App() {
   return (
     <div className="App">
      <h1>{greetings}</h1>
-
-     <Name />
+     <Nakama nama="Monkey D. Luffi" power="Gatling Gun"/>
+     <Nakama nama="Roronoa Zoro" power="Santoryu" />
     </div>
   );
 }
