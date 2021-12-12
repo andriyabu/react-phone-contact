92ebf225034c6f46e15ffe2399d77846f9449bd3
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2021-12-12 15:51:01 +0700

    add react-router-dom and import BrowserRouter, Routes and Route on App.js, import Link on Navbar and change a tag to Link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
 
+
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-     <Home />
-     <Create />
-     
-    </div>
+    <Router>
+        <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>  
+        </div>
+    </Router>
   );
 }
 
